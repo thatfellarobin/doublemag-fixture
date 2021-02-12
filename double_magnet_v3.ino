119016e7b0d54010b5848857bd4892ac0ded1fdc
Robin Liu <Robin.Liu.831@gmail.com>
2021-02-11 19:58:28 -0500

    some more work on arduino sketch

--- double_magnet_v3.ino
+++ double_magnet_v3.ino
@@ -4,9 +4,11 @@ Author: Robin Liu
 This sketch controls four stepper motors through EasyDriver or similar driver boards.
 It listens for serial input containing instructions for running the motors and executes those instructions.
 
-Notes on direction:
+Notes on movement:
 - Positive # of steps rotates the motor shaft clockwise as viewed from the motor body.
 - For the linear axis, clockwise rotation moves the magnet away from the workspace
+- Linear: 200 steps per rev / 8 mm per rev = 25 steps per mm
+- Angular: 200 steps per rev / 360 deg per rev = 0.56 steps per deg
 
 Attribution:
 - Code for serial interfacing from here: https://forum.arduino.cc/index.php?topic=288234.0
@@ -16,6 +18,7 @@ Attribution:
 #include <AccelStepper.h>
 #include <Wire.h>
 #include "Adafruit_MLX90393.h"
+#include <math.h>
 
 // PINS
 const int stepPins[] = {2, 4, 6, 8};
@@ -26,17 +29,17 @@ const int limitPins[] = {10, 11}; // Pull down
 // Serial message variables
 const byte msgNumChars = 32;
 char receivedChars[msgNumChars];
-long steps; // The absolute step count target received in the most recent message.
-byte activeMotor; // The motor commanded to move by the most recent message
 boolean newMsg = false; // flag to indicate if a new message has arrived
 boolean validMsg = false; // If the parser finds a valid message, this flag is flipped true
 
 // Data reporting variables
 unsigned long currentTime;
-unsigned long previousTime;
-const unsigned int timeInterval = 100; // milliseconds between outgoing messages
-float magfield_x, magfield_y, magfield_z; // Raw magnetic measurement values
-float magfield_magnitude, magfield_angle; // Derived field characteristics
+unsigned long previousReportTime;
+unsigned long previousMagsenseTime;
+const unsigned int reportingTimeInterval = 100; // milliseconds between outgoing messages
+const unsigned int magsenseTimeInterval = 50; // milliseconds between magnetic field sensor readings
+float magfield_x, magfield_y, magfield_z; // Raw magnetic measurement values TODO: Figure out which axis we don't care about.
+float magfield_magnitude, magfield_angle; // Derived field characteristics. These values are filtered.
 
 
 // Motor control
@@ -45,7 +48,6 @@ AccelStepper Motor_B(AccelStepper::DRIVER, stepPins[1], dirPins[1]);
 AccelStepper Motor_C(AccelStepper::DRIVER, stepPins[2], dirPins[2]);
 AccelStepper Motor_D(AccelStepper::DRIVER, stepPins[3], dirPins[3]);
 long motorStepTarget[motorCount] = {}; // stores the target motor positions relative to the startup state
-float speed_A, speed_B;
 int fluxTarget = 1;
 float angleTarget = 0;
 
@@ -90,6 +92,8 @@ void setup() {
 }
 
 void loop() {
+  currentTime = millis();
+
   // # Check for new messages in serial port
   recvWithEndMarker();
 
@@ -100,7 +104,10 @@ void loop() {
   }
 
   // # Take field measurement
-  getFieldValues();
+  if (currentTime - previousMagsenseTime > magsenseTimeInterval) {
+    previousMagsenseTime = currentTime;
+    getFieldValues();
+  }
 
   // # Decide on the target position
   // Consider the following:
@@ -109,51 +116,22 @@ void loop() {
   // 3. Other safety stops (field maxima, etc.)
 
   // (1) For a valid message, set motor target in accordance with the message
-  if (validMsg) {
-    setTargetFromField(fluxTarget, angleTarget);
-
-    // Make sure the command doesn't exceed the end stop.
-    if (digitalRead((limitPins[0]) && activeMotor == 0 || digitalRead(limitPins[1]) && activeMotor == 3) && steps > 0) {
-      // don't move the motor
-      Serial.print("endstopviolation,");
-      Serial.print(activemotor);
-      Serial.print("\r\n");
-    }
-    else { // No limit pin problems
-      motorStepTarget[activeMotor] = steps;
-      Motor_A.moveTo(motorStepTarget[0]);
-      Motor_B.moveTo(motorStepTarget[1]);
-      Motor_C.moveTo(motorStepTarget[2]);
-      Motor_D.moveTo(motorStepTarget[3]);
-    }
+  setStepTarget();
+  // (2) adjust target for limit switches.
+  if (digitalRead((limitPins[0]) && Motor_A.targetPosition()-Motor_A.currentPosition() > 0) {
+    Motor_A.stop();
   }
-  // (2, 3) adjust target for end stop violations and field safety.
-
-  else {
-    // Improper message format
-    Serial.println("nonvalidmsg\r\n");
+  if (digitalRead((limitPins[1]) && Motor_D.targetPosition()-Motor_D.currentPosition() > 0) {
+    Motor_D.stop();
   }
+  // TODO: (3) adjust target for field safety
 
-
-  // Limit switch instant stop
-  speed_A = Motor_A.speed();
-  speed_B = Motor_B.speed();
-  if (digitalRead(limitPins[0]) && speed_A > 0) {
-    // Stop motor A as soon as possible
-    Motor_A.runToPosition(Motor_A.currentPosition())
-  }
-  if (digitalRead(limitPins[0]) && speed_B > 0) {
-    // Stop motor D as soon as possible
-    Motor_B.runToPosition(Motor_A.currentPosition())
-  }
-
-  // Run motors -
+  // # Run motors -
   runMotors();
 
-  // Handle periodic data reporting
-  currentTime = millis();
-  if (currentTime - previousTime > timeInterval) {
-    previousTime = currentTime;
+  // # Handle periodic data reporting
+  if (currentTime - previousReportTime > reportingTimeInterval) {
+    previousReportTime = currentTime;
     writeTelemetry();
   }
 
@@ -185,6 +163,7 @@ void parseMsg() {
   Read through the received message
 
   TODO: Implement a stop signal
+  TODO: only update the global variables if the message is valid
 
   # Message descriptions:
   byte 1: one byte indicating the message type
@@ -243,39 +222,6 @@ void parseMsg() {
     // Stop immediately
     // TODO:
   }
-
-  // Loop through bytes
-  // for (int i = 0; i < msgNumChars; i++) {
-  //   if (i == 0) { // motor byte
-  //     activeMotor = receivedChars[i] - 'a';
-  //     if (activeMotor < 0 || activeMotor > 3) {
-  //       validMsg = false;
-  //       break;
-  //     }
-  //   }
-  //   else if (i > 0 && i < 7) { // step target bytes
-  //     newDigit = receivedChars[i] - '0';
-  //     if (newDigit >= 0 && newDigit <= 9) {
-  //       steps = (10 * steps) + newDigit;
-  //     }
-  //     else {
-  //       validMsg = false;
-  //       break;
-  //     }
-  //   }
-  //   else { // direction byte
-  //     if (receivedChars[i] == '-') {
-  //       steps = -steps;
-  //     }
-  //     else if (receivedChars[i] == '+') {
-  //       //do nothing
-  //     }
-  //     else {
-  //       validMsg = false;
-  //       break;
-  //     }
-  //   }
-  // }
 }
 
 void runMotors() {
@@ -286,28 +232,41 @@ void runMotors() {
   Motor_D.run();
 }
 
-void setTargetFromField(fluxTarget, angleTarget) {
+void setStepTarget() {
   // Proportional controller to set the target step value
   // Apply a tolerance to avoid vibrations.
+
+  float fieldDiff = fluxTarget - magfield_magnitude; // If positive, the motors need to move closer together (negative rotation)
+  float angleDiff = angleTarget - magfield_angle; // TODO:
+
+  long linearSteps = -20 * fieldDiff // 1 step for every 0.05 mT off from the target
+  long angularSteps = angleDiff
+
+  Motor_A.moveTo()
+  Motor_D.moveTo()
+  // float magfield_magnitude, magfield_angle; // Derived field characteristics. These values are filtered.
+  // int fluxTarget = 1;
+  // float angleTarget = 0;
 }
 
 void getFieldValues() {
+  int weight_old = 4; // weight applied to old value for weighted average (new value has a weight of 1)
   // Read magnetic sensor data
   // Calculate field magnitude and angle
   // Apply low pass filter - weighted average of previous value and new value
 
-
   if (sensor.readData(&magfield_x, &magfield_y, &magfield_z)) {
     // valid reading
-  }
-  else {
-    // invalid reading
+    float new_magfield_magnitude = sqrt(magfield_x*magfield_x + magfield_y*magfield_y + magfield_z*magfield_z); // TODO: verify correct axes used
+    float new_magfield_angle = atan2(magfield_y, magfield_x); // TODO: verify correct axes used
+
+    magfield_magnitude = (weight_old*magfield_magnitude + new_magfield_magnitude) / (weight + 1);
+    magfield_angle = (weight_old*magfield_angle + new_magfield_angle) / (weight + 1);
   }
 }
 
 void writeTelemetry() {
   // Send messages through serial to connected computer
-  // TODO:
 
   // Report motor positions
   Serial.print("steps,");
@@ -318,25 +277,11 @@ void writeTelemetry() {
   Serial.print(Motor_C.currentPosition());
   Serial.print(",");
   Serial.print(Motor_D.currentPosition());
-
-  // Take field measurement
-  if (sensor.readData(&magfield_x, &magfield_y, &magfield_z)) {
-    // Report field conditions
-    Serial.print(",");
-    Serial.print(magfield_x);
-    Serial.print(",");
-    Serial.print(magfield_y);
-    Serial.print(",");
-    Serial.print(magfield_z);
-  }
-  else {
-    // Always the same number of commas regardless of field measurement success
-    Serial.print(",")
-    Serial.print(",")
-    Serial.print(",")
-  }
-
+  // Report field conditions
+  Serial.print(",");
+  Serial.print(magfield_magnitude);
+  Serial.print(",");
+  Serial.print(magfield_angle);
   // End the message
   Serial.print("\r\n");
-}
 }
